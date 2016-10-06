43d33029f6cc25d9ca667719b721cd6418a6c0d5
App Academy Student <student@appacademy.io>
2016-10-06 16:03:15 -0700

    finish albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,26 +2,46 @@ class AlbumsController < ApplicationController
 
   def new
     @album = Album.new
-    render :
+    render :new
   end
 
   def create
-
-  end
-
-  def edit
-
+    @album = Album.new(album_params)
+    if @album.save
+      redirect_to album_url(@album)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :new
+    end
   end
 
   def show
+    @album = Album.find(params[:id])
+    render :show
+  end
 
+  def edit
+    @album = Album.find(params[:id])
+    render :edit
   end
 
   def update
-
+    @album = Album.find(params[:id])
+    if @album.update_attributes(album_params)
+      redirect_to album_url(@album)
+    else
+      flash.now[:errors] = @album.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
+    @album = Album.find(params[:id]).destroy
+    redirect_to band_url(@album.band)
+  end
 
+  private
+  def album_params
+    params.require(:album).permit(:name, :band_id, :yr, :live)
   end
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,10 +1,10 @@
 class Album < ApplicationRecord
 
-  validates :name, :band_id, :yr, :live, presence: true
+  validates :name, :band_id, :yr, presence: true
 
   belongs_to :band,
     primary_key: :id,
     foreign_key: :band_id,
     class_name: "Band"
-    
+
 end
